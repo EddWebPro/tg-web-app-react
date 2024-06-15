79f10992521de8ef4cb6cc722f2e6c38fbfc093b
EddWebPro <eduardihtiarov408@gmail.com>
2024-06-15 16:27:57 +0500

    +

--- tg-web-app-bot/index.js
+++ tg-web-app-bot/index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api');
+import TelegramBot from 'node-telegram-bot-api';
 
 const token = '7488084544:AAE2cb49874c7I9zhUD0fv90dAA1tD8Hpp4'
 const webAppUrl = 'https://main--dulcet-liger-a5df91.netlify.app/'
